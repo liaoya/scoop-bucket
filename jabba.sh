525244397ea35db5359fc3cb47338372acdfd57a
tom.shen <tom.shen@oracle.com>
2018-05-08 17:22:24 +0800

    Add jabba

---
+++ jabba.sh
@@ -0,0 +1,20 @@
+{
+    "homepage": "https://github.com/shyiko/jabba",
+    "checkver": {
+        "github": "https://github.com/shyiko/jabba"
+    },
+    "version": "0.10.1",
+    "url": "https://github.com/shyiko/jabba/releases/download/0.10.1/jabba-0.10.1-windows-amd64.exe",
+    "bin": [
+        [
+            "jabba-0.10.1-windows-amd64.exe",
+            "jabba.exe"
+        ]
+    ],
+    "persist": [
+        "jdk"
+    ],
+    "env_set": {
+        "JABBA_HOME": "$dir"
+    },
+}
